987b4fae7f0730321dcb876abeac6560c23de1d8
Junior sibusiso ngondo <67375679+Desire-cpu@users.noreply.github.com>
2023-04-01 23:18:12 -0700

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -1,31 +1,37 @@
 #include "main.h"
 
-/************************* WRITE HANDLE *************************/
+
 /**
  * handle_write_char - Prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
- * @width: get width.
- * @precision: precision specifier
+ * : char types.
+ * uffer: Buffer array to handle print
+ * flags:  Calculates active flags.
+ * width: get width.
+ * recision: precision specifier
  * @size: Size specifier
  *
  * Return: Number of chars printed.
  */
+
+
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
+{ /* cr is stored at left anpaddind at buffer's right */
 	int i = 0;
 	char padd = ' ';
+
 
 	UNUSED(precision);
 	UNUSED(size);
 
+
 	if (flags & F_ZERO)
 		padd = '0';
 
+
 	buffer[i++] = c;
 	buffer[i] = '\0';
+
 
 	if (width > 1)
 	{
@@ -47,16 +53,15 @@ int handle_write_char(char c, char buffer[],
 /************************* WRITE NUMBER *************************/
 /**
  * write_number - Prints a string
- * @is_negative: Lista of arguments
- * @ind: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
+ * is_negative: Lista of arguments
+ * ind: char types.
+ * buffer: Buffer array to handle print
+ * flags:  Calculates active flags
  *
  * Return: Number of chars printed.
  */
+
+
 int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -81,21 +86,26 @@ int write_number(int is_negative, int ind, char buffer[],
 /**
  * write_num - Write a number using a bufffer
  * @ind: Index at which the number starts on the buffer
- * @buffer: Buffer
- * @flags: Flags
+ * @bffer: Buffer
+ * @fags: Flags
  * @width: width
- * @prec: Precision specifier
- * @length: Number length
- * @padd: Pading char
- * @extra_c: Extra char
+ * @prc: Precisio specfier
+ * @length: Number legth
+ * @pad: Pading car
+ * xtra_c: Extra char
  *
- * Return: Number of printed chars.
+ * Reurn: Nuber of prited hars.
  */
+
+
+
 int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
 {
 	int i, padd_start = 1;
+
+
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 		return (0); /* printf(".0d", 0)  no char is printed */
@@ -136,6 +146,7 @@ int write_num(int ind, char buffer[],
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
 }
+
 
 /**
  * write_unsgnd - Writes an unsigned number
